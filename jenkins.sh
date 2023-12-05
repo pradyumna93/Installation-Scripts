d736c9c6341899cfdaf8b087f53acda95a8739f4
Pradyumna <75901112+pradyumna93@users.noreply.github.com>
2023-12-05 14:59:30 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,22 +2,19 @@
 
 sudo apt update -y
 
-sudo apt install openjdk-11-jre -y 
-
-java -version
+sudo apt install openjdk-11-jre -y
 
 sudo apt update -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt update -y
-
-sudo add-apt-repository universe -y
-
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
+sudo apt-get update -y
 sudo apt-get install jenkins -y
-
 sudo service jenkins start
 
 cat /var/lib/jenkins/secrets/initialAdminPassword
